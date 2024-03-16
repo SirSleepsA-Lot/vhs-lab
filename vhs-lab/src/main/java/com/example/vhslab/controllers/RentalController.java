b9d9745f03ba8da68f59e58337f1a5e81e77ea0e
antonio <antoniomocinic@gmail.com>
2024-03-16 17:09:25 +0100

    added controllers/RentalController.java

---
+++ vhs-lab/src/main/java/com/example/vhslab/controllers/RentalController.java
@@ -0,0 +1,16 @@
+package com.example.vhslab.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RentalController {
+    public RentalController() {
+    }
+
+    @GetMapping({"/"})
+    public String home() {
+        return "Welcome to the home page222!";
+    }
+}
